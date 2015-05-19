cccd021dfe7cb20bc91cfaf49b599be158c65897
Matan Frank <mtnfrnk@gmail.com>
2015-05-19 16:19:31 +0300

    function is_location_for_name function getallplace function getLocations

--- Crowdmap.py
+++ Crowdmap.py
@@ -6,27 +6,20 @@ class Crowdmap(object):
 
 	def get_all_posts_for(self, name):
 		return [post for post in self.list if post.find(name) != -1]
-
+	
 	def is_location_for_name(self, name):
 		name_posts = self.get_all_posts_for(name)
 		for post in name_posts:
 			if self.location_service.find(post): # if location_service.find(post):
 				return True
 		return False
-		
-	def getlocation(self,post):
-		return post.find("Bangkok")
 
-	def is_diff_location_for_name(self,name):
-		name_post=self.get_all_posts_for(name)
-		locations=self.location_service.getallplace(name)
-		return (len(locations)!=1)
 		
 class LocationService:
 	def find(self, text):
 		return (text.find("Bangkok") != -1 or text.find("Langtang") != -1)
 		
-	def getallplace(self, name):
+			def getallplace(self, name):
 		list=[]
 		for post in name:
  			if self.find(post):
