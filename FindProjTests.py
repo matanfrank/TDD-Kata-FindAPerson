d5f4b8157c2874b169661708345c323634a20fdd
Matan Frank <mtnfrnk@gmail.com>
2015-05-19 16:22:08 +0300

    function test_get_all_posts_for_missing_name

--- FindProjTests.py
+++ FindProjTests.py
@@ -8,19 +8,11 @@ class FindProjTests(unittest.TestCase):
 	def test_get_all_posts_for_name(self):
 		posts= self.crowdmap.get_all_posts_for("Or A.")
 		self.assertEquals(["I met Or A. at Chabad house Bangkok","We found Or A. R.I.P at Langtang valley"],posts)
-
 	
 	def test_get_all_posts_for_missing_name(self):
 		posts = self.crowdmap.get_all_posts_for("Joe")
 		self.assertEquals([],posts)
-		
-	def test_check_if_location_in_post(self):
-		location_exist=self.crowdmap.is_location_for_name("Or")
- 		self.assertTrue(location_exist)
-		
-	def test_check_if_diff_location_in_post(self):
-		location_exist=self.crowdmap.is_diff_location_for_name("Or")
- 		self.assertTrue(location_exist)
+
 
 if __name__ == '__main__':
 	unittest.main()
